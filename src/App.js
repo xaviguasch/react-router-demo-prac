438120444d64741948e245dfedc2ea22d3d278e4
xaviguasch <xaviguasch@gmail.com>
2019-05-11 21:28:09 +0200

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import About from './About'
 import Dog from './Dog'
 import Contact from './Contact'
 import './App.css'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      page: 'about'
-    }
-  }
-
-  changePage(newPage) {
-    this.setState({ page: newPage })
-  }
-
-  renderPage() {
-    if (this.state.page === 'about') return <About />
-    if (this.state.page === 'dog') return <Dog />
-    if (this.state.page === 'contact') return <Contact />
-  }
-
   render() {
     return (
       <div className='App'>
-        <nav className='App-nav'>
-          <a onClick={() => this.changePage('about')}>About</a>
-          <a onClick={() => this.changePage('dog')}>Dog</a>
-          <a onClick={() => this.changePage('contact')}>Contact</a>
-        </nav>
-        {this.renderPage()}
+        <Route path='/dog' component={Dog} />
       </div>
     )
   }
