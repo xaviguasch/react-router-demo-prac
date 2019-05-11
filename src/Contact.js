a0e0695b40631094795f3d51583af1079238f287
xaviguasch <xaviguasch@gmail.com>
2019-05-11 20:02:36 +0200

    added starter files

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contact</h1>
+        <p>please don't contact us ever</p>
+      </div>
+    )
+  }
+}
+
+export default Contact
---
+++ src/Dog.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Dog extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dog</h1>
+        <img
+          src='https://www.akc.org/wp-content/themes/akc/component-library/assets/img/welcome.jpg'
+          alt=''
+        />
+      </div>
+    )
+  }
+}
+
+export default Dog
