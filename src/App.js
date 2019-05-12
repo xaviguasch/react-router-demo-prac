5cb422ef11f2f31710c83772aed63ebab08dd2c2
xaviguasch <xaviguasch@gmail.com>
2019-05-12 18:28:24 +0200

    toyed with component vs render

--- src/App.js
+++ src/App.js
@@ -5,25 +5,39 @@ import Dog from './Dog'
 import Contact from './Contact'
 import './App.css'
 
+const Hater = () => <h1>I ABSOLUTELY HATE DOGS!</h1>
+
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <nav className='App-nav'>
-          <NavLink exact activeClassName='active-link' to='/'>
+          <NavLink activeClassName='active-link' to='/'>
             About
           </NavLink>
-          <NavLink exact activeClassName='active-link' to='/dog'>
-            Dog
-          </NavLink>
-          <NavLink exact activeClassName='active-link' to='/contact'>
+          <NavLink activeClassName='active-link' to='/contact'>
             Contact
           </NavLink>
+          <NavLink activeClassName='active-link' to='/dog'>
+            Dog
+          </NavLink>
+
+          <NavLink activeClassName='active-link' to='/dog/r'>
+            Dog(r)
+          </NavLink>
+          <NavLink activeClassName='active-link' to='/dog/c'>
+            Dog(c)
+          </NavLink>
         </nav>
+
         <Switch>
           <Route exact path='/' component={About} />
-          <Route exact path='/dog' component={Dog} />
           <Route exact path='/contact' component={Contact} />
+          <Route exact path='/dog' component={Dog} />
+          <Route exact path='/dog/hater' component={Hater} />
+
+          <Route exact path='/dog/c' component={() => <Dog name='Muffins' />} />
+          <Route exact path='/dog/r' render={() => <Dog name='Biscuits' />} />
         </Switch>
       </div>
     )
