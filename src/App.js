307978d474e57f6bc804ae6bb7db9d890d7ebf98
xaviguasch <xaviguasch@gmail.com>
2019-05-12 16:22:52 +0200

    added switch, route and link component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch, Link } from 'react-router-dom'
 import About from './About'
 import Dog from './Dog'
 import Contact from './Contact'
@@ -9,7 +9,16 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Route path='/dog' component={Dog} />
+        <nav className='App-nav'>
+          <Link to='/'>About</Link>
+          <Link to='/dog'>Dog</Link>
+          <Link to='/contact'>Contact</Link>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/dog' component={Dog} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
       </div>
     )
   }
